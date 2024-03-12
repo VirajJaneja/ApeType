d430076368a95a5da639c836ab598956de58c724
VirajJ <97782243+VirajJaneja@users.noreply.github.com>
2024-03-11 20:39:32 -0400

    tiny fix

--- script.js
+++ script.js
@@ -12,6 +12,16 @@ function printTest(){
     // return generate();
 }
 
+function apeTypeMenuB(){
+    console.log("Mode switched to " + mode);
+    mode = "Base";
+}
+
+function HellDiversMenuB(){
+    console.log("Mode switched to " + mode);
+    mode = "HellDivers";
+}
+
 function genRandomColor(){
     console.log("reached");
     const arrayOfColorFunctions = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
